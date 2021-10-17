730cdf94790af676e2a4bcf6499e2de81d432a1a
WuDiDaBinGe <1151680016@qq.com>
2021-10-17 22:07:49 +0800

    尝试预加载word embedding

--- start_train.sh
+++ start_train.sh
@@ -1,5 +1,5 @@
 # train atm
-python main_train.py --taskname  goriler  --n_topic 30 --num_epochs 50000 --language en --clean_data True --batch_size 256 --lr 1e-4 --n_critic 10
+python main_train.py --taskname  goriler  --n_topic 100 --num_epochs 50000 --language en --clean_data True --batch_size 256 --lr 1e-4 --n_critic 10
 # python main_train.py --taskname  20news_clean  --n_topic 20 --num_epochs 50000 --language en --clean_data True --batch_size 256 --bkpt_continue True
 
 # train c_atm
